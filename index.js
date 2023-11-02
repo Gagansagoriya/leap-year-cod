5607ac44878129ec401f709456b66e76e34cc244
Gagan Sagoriya <135191304+Gagansagoriya@users.noreply.github.com>
2023-11-02 19:35:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+// practice -set -question
+
+
+// console.log(hello-world);
+// var myname = 'gagan mehara';
+// console.log(myname);
+// var _myname = "vinod";
+// var _1my__Name = "bahadur";
+// var 1myname = "thapa";
+// var $myname = "thapa technical";
+// console.log(_myname , _1my__Name ,$myname);
+
+// typeof operator\
+// console.log(typeof(myname));
+// var myAge = 26;
+// console.log(myAge);
+// console.log(typeof(myAge));
+// console.log(10 + "10");
+// console.log(9 - "5");
+// console.log("java" + "script");
+// console.log("vonod" -"thapa");
+// console.log(true + true);
+// var myPhoneMo = globalT
+// console.log(true+false);
+// console.log(false - true);
+
+// interview Question
+// Diffrence between null vs undefinded
+// var imNull = null;
+// console.log(imNull);
+// console.log(typeof(imNull));
+
+// var imundefinded;
+// console.log(imundefinded);
+// console.log(typeof(imundefinded));
+
+// var phoneNo = 98274286358;
+// var name = "Sahil chacha";
+
+// console.log(isNaN(phoneNo));
+// console.log(isNaN(name));
+// if(isNaN(name)){
+//     console.log("please enter a valid number");
+// }
+
+// console.log(NaN === NaN);
+// console.log(Number.NaN === NaN);
+// console.log(isNaN(NaN));
+// console.log(Number.isNaN(NaN));
+// var x = 6;
+// var y = 6;
+
+// console.log('is x and y are both equal :  ${x == y}');
+// post indcrement
+// var num = 5;
+// var newnum = num++;
+// console.log(newnum);
+// console.log(num);
+
+// pre increment
+// var num = 5;
+// var newnum =++num;
+// console.log(newnum);
+// console.log(num);
+
+// post Decrement
+
+// var  a = 3;
+// var b = 4;
+// var tommorow;
+// if(tommorow == 'rain'){
+//     console.log("Take u a raincote");
+// } else{
+//     console.log("no need to take a raincote");
+// }
+ var  year = 2020;
+ if ((year%4==0) && ((year%400==0) || (year%100!==0))){
+  console.log("This year " + year +" is a leap year");
+ } else{
+console.log("This year " +year + "  is not a Leap year");
+ }
